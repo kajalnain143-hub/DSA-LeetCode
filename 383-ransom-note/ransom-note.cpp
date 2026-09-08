59d036511d76a836edeec4c2a0cf34db9be06f4e
kajalnain143-hub <kajalnain143@gmail.com>
2026-09-08 22:21:58 +0530

    Time: 2 ms (58.29%) | Memory: 12 MB (29.44%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>mp;
+        for(char ch : magazine){
+            mp[ch]++;
+        }
+         for(char ch : ransomNote){
+            mp[ch]--;
+        }
+        for(auto i : mp){
+            if(i.second<0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
